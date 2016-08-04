52394fa4e290556da6a03862323755273357445e
DreeMorr <deirdre.a.morrison@gmail.com>
2016-08-04 20:35:48 -0300

    intial installation script

---
+++ asciiquarium.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Script for installing Asciiquarium 
+
+
+#Install dependencies.
+
+sudo apt-get install libcurses-perl
+cd /tmp
+wget http://search.cpan.org/CPAN/authors/id/K/KB/KBAUCOM/Term-Animation-2.6.tar.gz
+tar -zxvf Term-Animation-2.6.tar.gz
+cd Term-Animation-2.6
+perl Makefile.PL && make && make test
+sudo make install
+
+
+#Install Asciiquarium 
+cd /tmp
+wget http://www.robobunny.com/projects/asciiquarium/asciiquarium
+chmod 777 asciiquarium
+./asciiquarium 
