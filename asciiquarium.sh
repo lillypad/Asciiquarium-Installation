bad8c0820011005d2a9fdbc7efa7fd5f7a47e9c8
Lilly Chalupowski <lchalupowski@gosecure.ca>
2016-08-05 08:09:26 -0300

    Some fixes
    Update sources, don't assume perl is installed, automatic yes to apt-get installs.
    Install asciiquarium to opt folder.

--- asciiquarium.sh
+++ asciiquarium.sh
@@ -3,7 +3,8 @@
 #Script for installing Asciiquarium 
 
 #Install dependencies.
-sudo apt-get install libcurses-perl
+sudo apt-get update
+sudo apt-get -y install perl libcurses-perl
 cd /tmp
 wget http://search.cpan.org/CPAN/authors/id/K/KB/KBAUCOM/Term-Animation-2.6.tar.gz
 tar -zxvf Term-Animation-2.6.tar.gz
@@ -12,6 +13,8 @@ perl Makefile.PL && make && make test
 sudo make install
 
 #Install Asciiquarium 
+mkdir /opt/asciiquarium
+cd /opt/asciiquarium
 wget http://www.robobunny.com/projects/asciiquarium/asciiquarium
 chmod 777 asciiquarium
 ./asciiquarium 
